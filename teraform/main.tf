4043717abdabf031781d44a65240067b17183819
Logezz-07 <logeshwaran.b@servion.com>
2025-11-11 23:57:26 +0530

    new

--- teraform/main.tf
+++ teraform/main.tf
@@ -1,11 +1,15 @@
 terraform {
+  backend "gcs" {}
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
   }
+  
   required_version = ">= 1.6.0"
+
 }
 
 provider "google" {
